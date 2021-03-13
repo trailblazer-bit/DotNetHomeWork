b6e2566109556fd4bfe59b6acd68a4ef7b691ffc
Jialong <1552217634@qq.com>
2021-03-13 14:27:38 +0800

    第三次作业

---
+++ hwk03/3.1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape square = new Square(8);
+            Console.WriteLine("正方形面积为:"+square.CalculateArea());
+            Console.WriteLine(square.IsLegal());
+
+            IShape triangle = new Triangle(12, 56,48);
+            Console.WriteLine("三角形面积为:"+triangle.CalculateArea());
+            Console.WriteLine(triangle.IsLegal());
+
+            IShape rectangle = new Rectangle(12, 34);
+            Console.WriteLine("长方形面积为:"+rectangle.CalculateArea());
+            Console.WriteLine(rectangle.IsLegal());
+        }
+    }
+
+
+    public interface IShape
+    {
+        //计算面积
+        double CalculateArea();
+
+        //判断形状是否合法
+        bool IsLegal();
+    }
+
+    //长方形类
+    public class Rectangle : IShape
+    {
+        public double Width
+        {
+            get;set;
+        }
+
+        public double Height
+        {
+            get;set;
+        }
+
+        public Rectangle(double width,double height)
+        {
+            this.Width = width;
+            this.Height = height;
+            if (!IsLegal())
+                throw new ArgumentException("输入的长方形的长或宽不合法!");
+        }
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public  bool IsLegal()
+        {
+            return Width > 0 && Height > 0;
+        }
+    }
+    //三角形类
+    public class Triangle : IShape
+    {
+        public double Side1 { get;set;}
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+
+        public Triangle(double side1,double side2,double side3)
+        {
+            this.Side1 = side1;
+            this.Side2 = side2;
+            this.Side3 = side3;
+            if (!IsLegal())
+                throw new ArgumentException("输入的三角形三边不合法！");
+        }
+        public double CalculateArea()
+        {
+            double p = (Side1 + Side2 + Side3) / 2;
+            double area = Math.Sqrt(p * (p - Side1) * (p - Side2) * (p - Side3));
+            return area;
+        }
+
+        public bool IsLegal()
+        {
+            bool flag1, flag2, flag3;
+            flag1 = (Side1 + Side2) > Side3;
+            flag2 = (Side2 + Side3) > Side1;
+            flag3 = (Side1 + Side3) > Side2;
+
+            return flag1 && flag2 && flag3 && Side1 > 0 && Side2 > 0&&Side3>0 ;
+        }
+    }
+    
+    //正方形类
+    public class Square :IShape
+    {
+        public double Side { get; set; }
+        public Square(double side)
+        {
+            this.Side = side;
+            if (!IsLegal())
+                throw new ArgumentException("输入的正方形的边长不合法");
+        }
+
+        public  double CalculateArea()
+        {
+            return this.Side * this.Side;
+        }
+
+        public bool IsLegal()
+        {
+            return this.Side > 0;
+        }
+
+    }
+
+}
---
+++ hwk03/3.2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using _3._1;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            double totalArea = 0.0;
+            int kind;
+            ShapeFactory shapeFactory = new ShapeFactory();
+
+            for (int i=0;i<10;i++)
+            {
+                kind = r.Next(0, 3);
+                double []arr=InputShapeInfo(kind);
+
+                totalArea += shapeFactory.GetShape(kind, arr).CalculateArea();
+            }
+            Console.WriteLine("面积总和为:"+totalArea);
+        }
+        //输入相关的形状信息,并且返回某个形状的边长数组
+        static double[] InputShapeInfo(int kind)
+        {
+            if (kind == 0)
+                Console.WriteLine("请输入长方形的长和宽:");
+            else if (kind == 1)
+                Console.WriteLine("请输入正方形的边长:");
+            else if (kind == 2)
+                Console.WriteLine("请输入三角形的三边长:");
+            else
+                ;
+            String[] nums = Console.ReadLine().Split(" ");
+            double[] arr = new double[nums.Length];
+            for (int j = 0; j < nums.Length; j++)
+                arr[j] = Double.Parse(nums[j]);
+            return arr;
+        }
+    }
+
+
+    public class ShapeFactory
+    {
+        //工厂模式，生产长方形，正方形，三角形形状的对象
+        public  IShape GetShape(int kind, params double[] arr)
+        {
+            IShape shape = null;
+            switch (kind)
+            {
+                case 0:
+                    {
+                        IsParamsLegal(arr);
+                        shape = new Rectangle(arr[0], arr[1]);
+                        break;
+                    }
+                case 1:
+                    {
+                        IsParamsLegal(arr);
+                        shape = new Square(arr[0]);
+                        break;
+                    }
+                case 2:
+                    {
+                        IsParamsLegal(arr);
+                        shape = new Triangle(arr[0], arr[1], arr[2]);
+                        break;
+                    }
+
+                default: break;
+            }
+            return shape;
+        }
+        //在每次创建一个形状对象之前都进行参数合法性验证
+        public static void  IsParamsLegal(params double[]arr)
+        {
+            bool flag = true;
+            if (arr == null || arr.Length ==0)
+                flag=false;
+            else
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    if (arr[i] <= 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+            }
+            if (!flag)
+                throw new ArgumentException("多边形的边长参数不合法!!!");
+        }
+    }
+}
