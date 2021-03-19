4c28738002bab54b70131e51c5a07938e00034c8
Jialong <1552217634@qq.com>
2021-03-19 17:52:27 +0800

    第四次作业

---
+++ hwk04/4_1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace _4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> nodeList = new GenericList<int>();
+            //初始化链表
+            for(int i=1;i<=10;i++)
+            {
+                nodeList.Add(i);
+            }
+            //遍历链表
+            nodeList.Foreach((Node<int> node) => Console.Write(node.Data+" "));
+            Console.WriteLine();
+            //求出链表中元素之和
+            int sum = 0;
+            nodeList.Foreach((node) => sum += node.Data);
+            Console.WriteLine("sum={0}",sum);
+
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+            //求出链表中元素的最大值
+            nodeList.Foreach((node) => 
+            {
+                if (max < node.Data)
+                    max = node.Data;
+            });
+            Console.WriteLine("maxValue={0}",max);
+
+            //求出链表中元素的最小值
+            nodeList.Foreach((node) =>
+            {
+                if (min > node.Data)
+                    min = node.Data;
+            });
+            Console.WriteLine("minValue={0}", min);
+        }
+    }
+
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //链表泛型类
+    public class GenericList<T>
+    {
+        //定义头结点和尾结点
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            this.head = this.tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if(tail==null)
+            {
+                this.head = this.tail = node;
+            }
+            else
+            {
+                this.tail.Next = node;
+                this.tail = node;
+            }
+        }
+
+        //自定义泛型链表的Foreach方法
+        //通过Action这个内置委托对象，实现对链表中的每个元素实行一定的操作
+        public  void Foreach(Action<Node<T>> action)
+        {
+            Node<T> p = this.head;
+            while(p!=null)
+            {
+                action(p);
+                p = p.Next;
+            }
+        }
+    }
+}
---
+++ hwk04/4_2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading;
+
+namespace _4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.OnTick += new TickHandler(Program.Clock_Tick);
+            clock.OnAlarm += new AlarmHandler(Program.Clock_Alarm);
+            Console.WriteLine("请设置闹钟的时间(时，分，秒)");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            int minute = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            clock.AlarmTime = new Time(hour, minute, second);
+
+            clock.Start();
+
+        }
+        static void Clock_Tick(object sender,Time time)
+        {
+            Console.WriteLine("现在是{0}:{1}:{2}", time.Hour, time.Minute, time.Second);
+        }
+        static void Clock_Alarm(object sender,Time time)
+        {
+            Console.WriteLine("时间到了，该做事情了");
+        }
+    }
+    //定义时间类型
+    public class Time
+    {
+        public Time(int hour, int minute, int second)
+        {
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public int Hour
+        {
+            get; set;
+        }
+        public int Minute
+        {
+            get; set;
+        }
+        public int Second
+        {
+            get; set;
+        }
+
+        //重写Equals方法
+        public override bool Equals(object obj)
+        {
+            return obj is Time time &&
+                   Hour == time.Hour &&
+                   Minute == time.Minute &&
+                   Second == time.Second;
+        }
+    }
+
+    //定义两个委托类型，分别监听滴答和响铃事件
+    public delegate void TickHandler(object sender,Time time);
+    public delegate void AlarmHandler(object sender,Time time);
+
+
+    class Clock
+    {
+        //定义两个委托实例，一个tick，一个alarm
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+
+        private Time currentTime;
+
+        private Time alarmTime;
+        
+        public Clock()
+        {
+            //使用系统当前时间
+            DateTime time = System.DateTime.Now;
+            currentTime = new Time(time.Hour, time.Minute, time.Second);
+            
+        }
+  
+        public Time AlarmTime
+        {
+            get { return alarmTime; }
+            //闹钟的合法性检查
+            set
+            {
+                bool flag1 = value.Hour < 0 || value.Hour >= 24;
+                bool flag2 = value.Minute < 0 || value.Minute >= 60;
+                bool flag3 = value.Second < 0 || value.Second >= 60;
+                if (flag1 || flag2 || flag3)
+                    throw new ArgumentException("设置的闹钟时间非法!!");
+                this.alarmTime = value;
+            }
+        }
+
+        public Time CurrentTime
+        {
+            get => currentTime;
+        }
+
+
+
+        public void Tick()
+        {
+            DateTime time = System.DateTime.Now;
+            currentTime.Hour = time.Hour;
+            currentTime.Minute = time.Minute;
+            currentTime.Second = time.Second;
+            OnTick(this,currentTime);
+        }
+
+        public void Alarm()
+        {
+            OnAlarm(this,currentTime);
+        }
+
+        //开启闹钟
+        public void Start()
+        {
+            while(true)
+            {
+                //每隔一秒闹钟滴答一次
+                Thread.Sleep(1000);
+                Tick();
+                if(alarmTime!=null&&currentTime.Equals(alarmTime))
+                {
+                    Alarm();
+                    //break;
+                }
+            }
+        }
+    }
+}
