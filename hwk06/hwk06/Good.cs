13034401f9bd9c726fd1529b9d5022f9dd30feb8
Jialong <1552217634@qq.com>
2021-04-03 19:25:40 +0800

    序列化和反序列，单元测试

---
+++ hwk06/hwk06/Good.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderManageSystem
+{
+    public class Good
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Good(string name,double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "商品名称:" + Name + " 商品价格:" + Price+"\n";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Good good = obj as Good;
+            return good!=null &&
+                   Name == good.Name &&
+                   Price == good.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+    }
+}
---
+++ hwk06/hwk06Test/OderSystemTest.cs
@@ -0,0 +1,159 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OrderManageSystem;
+using System;
+
+namespace hwk06Test
+{
+    [TestClass]
+    public class OderSystemTest
+    {
+        List<Order> orders = new List<Order>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            Good good1 = new Good("ÀºÇò", 56);
+            Good good2 = new Good("×ãÇò", 78);
+            Good good3 = new Good("ÅÅÇò", 34);
+
+            Customer customer1 = new Customer("Jack");
+            Customer customer2 = new Customer("Nike");
+
+            Order order1 = new Order(1, customer1.Name, System.DateTime.Now);
+            Order order2 = new Order(2, customer2.Name, System.DateTime.Now);
+            Order order3 = new Order(3, customer2.Name, System.DateTime.Now);
+
+            order1.AddDetail(new OrderDetail(good1, 7));
+            order2.AddDetail(new OrderDetail(good1, 7));
+            order2.AddDetail(new OrderDetail(good2, 9));
+            order3.AddDetail(new OrderDetail(good1, 7));
+            order3.AddDetail(new OrderDetail(good2, 9));
+            order3.AddDetail(new OrderDetail(good3, 12));
+
+            orders.Add(order1);
+            orders.Add(order2);
+            orders.Add(order3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestAddOrder()
+        {
+            OrderService service = new OrderService();
+
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+            service.AddOrder(orders[0]);
+            CollectionAssert.AreEqual(service.OrderList, orders);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestRemoveOrder()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+            service.RemoveOrder(orders[2].ID);
+            service.RemoveOrder(orders[2].ID);
+
+            orders.Remove(orders[2]);
+            CollectionAssert.AreEqual(service.OrderList, orders);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestModifyOrder()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[1]);
+
+            service.ModifyOrder(orders[1].ID, orders[2]);
+            orders.Remove(orders[1]);
+            CollectionAssert.AreEqual(orders, service.OrderList);
+        }
+
+        [TestMethod]
+        public void TestQueryOrderById()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+
+            Order o = service.QueryOrderById(orders[1].ID);
+            Assert.AreEqual(o, orders[1]);
+        }
+
+
+        [TestMethod]
+        public void TestQueryOrderByGoodName()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+
+            List<Order> os = service.QueryOrderByGoodName("ÀºÇò");
+            CollectionAssert.AreEqual(os, orders);
+        }
+
+        [TestMethod]
+        public void TestQueryOrderByCustomer()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+
+            List<Order> os = service.QueryOrderByCustomer("Nike");
+            orders.Remove(orders[0]);
+            CollectionAssert.AreEqual(os, orders);
+        }
+
+        [TestMethod]
+        public void TestQueryOrderByTotalPrice()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+
+            List<Order> os = service.QueryOrderByTotalPrice(orders[0].TotalPrice);
+            orders.Remove(orders[2]);
+            orders.Remove(orders[1]);
+            CollectionAssert.AreEqual(os, orders);
+        }
+        [TestMethod]
+
+        public void TestSort()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[2]);
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.Sort();
+
+            CollectionAssert.AreEqual(orders, service.OrderList);
+
+        }
+
+        [TestMethod]
+        public void TestShowOrders()
+        {
+            OrderService service = new OrderService();
+            service.AddOrder(orders[0]);
+            service.AddOrder(orders[1]);
+            service.AddOrder(orders[2]);
+
+            service.ShowOrder();
+
+        }
+    }
+}
